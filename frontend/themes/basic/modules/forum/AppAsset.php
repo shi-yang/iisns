18cb45a33e4ee0b0550bd98144b15f8d1b127bbe
shi-yang <dr@shiyang.me>
2015-03-08 13:59:03 +0800

    解决不能自动加载bootstrap.js

--- frontend/themes/basic/modules/forum/AppAsset.php
+++ frontend/themes/basic/modules/forum/AppAsset.php
@@ -25,6 +25,6 @@ class AppAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
     ];
 }
