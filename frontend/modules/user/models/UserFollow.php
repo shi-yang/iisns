81f13d4e21152bd6c3b732d5c131dbae5a2fb85d
shi-yang <dr@shiyang.me>
2015-03-22 17:32:13 +0800

    Update

---
+++ frontend/modules/user/models/UserFollow.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\modules\user\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%user_follow}}".
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $people_id
+ *
+ * @property User $user
+ * @property User $people
+ */
+class UserFollow extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%user_follow}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'people_id'], 'required'],
+            [['user_id', 'people_id'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'people_id' => Yii::t('app', 'People ID'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPeople()
+    {
+        return $this->hasOne(User::className(), ['id' => 'people_id']);
+    }
+}
