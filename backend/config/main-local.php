86bda7968ac34e39a7583a6f5f6e2999cbbd8260
shi-yang <dr@shiyang.me>
2015-05-03 00:03:19 +0800

    change to prod

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -1,21 +1,9 @@
 <?php
-
-$config = [
+return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'ISYWEGLGFz2DW83Uf_JA1AcV_N5i76bb',
+            'cookieValidationKey' => 'mOfe242T8Wpt8HBUzb8t4H4dQ-tn-pP1',
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
-}
-
-return $config;
