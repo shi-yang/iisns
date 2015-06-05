20c4368152bc00ea68e8ee6fd1312df8cdb47344
Shiyang <admin@iisns.com>
2015-06-05 08:33:24 +0800

    修改在宽屏下的显示

--- frontend/themes/basic/explore/photos.php
+++ frontend/themes/basic/explore/photos.php
@@ -81,7 +81,7 @@ $this->registerCss('
             $albumUrl = Url::toRoute(['/explore/view-album', 'id' => $model['id']]);
             $src = (empty($model['path'])) ? Yii::getAlias('@web/images/pic-none.png') : Yii::getAlias('@photo') . $model['path'] ;
         ?>
-        <div class="col-xs-6 col-sm-4 col-md-3">
+        <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
             <div class="photo-item">
                 <a title="<?= Html::encode($model['name']) ?>" href="<?= Yii::getAlias('@photo').$model['path']?>" data-lightbox="image-1" data-title="<?= Html::encode($model['name']) ?>">
                     <img src="<?= $src ?>" class="img-responsive" alt="photo-cover">
