0dd9d446f106d50a52055449900e1ec4beecfe3b
Shiyang <admin@iisns.com>
2015-06-20 23:59:49 +0800

    Updated

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -1,21 +1,9 @@
 <?php
-
-$config = [
+return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '2mdpU1wC9hZa8X-I7hshGCF-kW6A5-b9',
+            'cookieValidationKey' => '2n3QOP5IvL4m0m4nnzryXm-tQ5u7UEVe',
         ],
     ],
 ];
-
-if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
-}
-
-return $config;
