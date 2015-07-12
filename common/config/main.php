c0c041c10a0d20cd2f17957fb3e9608b633143c8
Shiyang <admin@iisns.com>
2015-07-12 15:09:28 +0800

    在生产模式中使用cdn

--- common/config/main.php
+++ common/config/main.php
@@ -10,12 +10,23 @@ return [
             'class' => 'common\components\Setting',
         ],
         'assetManager' => [
-            'forceCopy' => (YII_DEBUG) ? true : false
+            'forceCopy' => YII_DEBUG ? true : false,
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'jquery.js' : '//cdn.bootcss.com/jquery/2.1.4/jquery.min.js',
+                    ]
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [
+                        YII_ENV_DEV ? 'css/bootstrap.css' : '//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css',
+                    ]
+                ],
+            ],
         ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName'=>false
         ],
     ],
-   
 ];
