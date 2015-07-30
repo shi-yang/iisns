9334b9052b6a189f9f1ed8a69872394868516ccf
Shiyang <admin@iisns.com>
2015-07-31 05:01:33 +0800

    修改日志内容管理页面

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -4,7 +4,7 @@ namespace backend\controllers;
 
 use Yii;
 use backend\models\Post;
-use yii\data\Pagination;
+use backend\models\PostSearch;
 use common\components\BaseController;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -32,16 +32,12 @@ class PostController extends BaseController
      */
     public function actionIndex()
     {
-        $query = Post::find();
-        $countQuery = clone $query;
-        $pages = new Pagination(['totalCount' => $countQuery->count()]);
-        $models = $query->offset($pages->offset)
-            ->limit($pages->limit)
-            ->all();
+        $searchModel = new PostSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'models' => $models,
-            'pages' => $pages
+            'dataProvider' => $dataProvider,
+            'searchModel' => $searchModel,
         ]);
     }
 
