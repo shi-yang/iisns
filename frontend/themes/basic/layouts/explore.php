09ccd075dbf856bb6c9e37478c250c551e33e1b7
shi-yang <dr@shiyang.me>
2015-12-24 17:44:20 +0800

    为两个链接关闭pjax功能

--- frontend/themes/basic/layouts/explore.php
+++ frontend/themes/basic/layouts/explore.php
@@ -27,9 +27,17 @@ NavBar::begin([
 ]);
 $menuItems = [
     ['label' => '<i class="glyphicon glyphicon-globe"></i> ' . Yii::t('app', 'Home'), 'url' => ['/explore/index']],
-    ['label' => '<i class="glyphicon glyphicon-picture"></i> ' . Yii::t('app', 'Photos'), 'url' => ['/explore/photos']],
+    [
+        'label' => '<i class="glyphicon glyphicon-picture"></i> ' . Yii::t('app', 'Photos'),
+        'url' => ['/explore/photos'],
+        'linkOptions' => ['data-pjax' => 0]
+    ],
     ['label' => '<i class="glyphicon glyphicon-list-alt"></i> ' . Yii::t('app', 'Posts'), 'url' => ['/explore/posts']],
-    ['label' => '<i class="glyphicon glyphicon-comment"></i> ' . Yii::t('app', 'Forums'), 'url' => ['/explore/forums']],
+    [
+        'label' => '<i class="glyphicon glyphicon-comment"></i> ' . Yii::t('app', 'Forums'),
+        'url' => ['/explore/forums'],
+        'linkOptions' => ['data-pjax' => 0]
+    ],
 ];
 echo Nav::widget([
     'options' => ['class' => 'navbar-nav'],
